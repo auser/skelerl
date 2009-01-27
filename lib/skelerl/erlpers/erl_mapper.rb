f1cd3660fc65e72e334d35fee19859603a004566
Ari Lerner <alerner@YPCMC05591.local>
2009-01-27 10:26:11 -0800

    Added more mapper methods, added namespacing and string building

--- lib/skelerl/erlpers/erl_mapper.rb
+++ lib/skelerl/erlpers/erl_mapper.rb
@@ -6,11 +6,19 @@ class ErlMapper
   end
   
   default_options({
-    :path => ["pa", "./ebin"],
-    :sname => ["sname", "node0"],
+    :path => "./ebin",
+    :sname => "node0",
+    :cookie => nil,
     :stop => true,
     :testing => true
   })
+  
+  def self.erl_methods(hash={})
+    puts "in erl_methods with #{hash}"
+    hash.each {|k,v| define_method "erl_#{k}" do;"#{v}";end}
+  end
+  
+  erl_methods(:path => "pa", :cookie => "setcookie")
   
   def options(opts={}, &block)
     set_vars_from_options opts
@@ -18,23 +26,56 @@ class ErlMapper
   end
   
   def with_node(name="node0", opts={}, &block)
-    m = MappingContext.new name
-    parent.dsl_options.each {|k,v| m.take_options(v)}
+    MappingContext.new name, __options, &block
+  end
+  
+end
+
+class MappingContext < ErlMapper
+  attr_accessor :sname, :parent
+  
+  def initialize(name, opts={}, &block)
+    @sname = name
+    @opts = opts
+    instance_eval &block if block
+  end
+  
+  def namespace name, &block
+    Namespace.new(name, @opts, &block)
+  end
+  
+  def string
+    "#{build_string} #{start_commands} #{final_commands}"
+  end
+  
+  def final_commands
+    returning Array.new do |arr|
+      arr << "-s init stop" if opts.has_key?(:stop) && opts[:stop]
+    end.join(" ")
+  end
+    
+  def build_string
+    @build_string ||= 
+      "erl #{opts.collect {|k,v| "-#{get_opt_name(k)} #{v} " if v && v.is_a?(String)}}"
+  end
+  
+  def get_opt_name(k)
+    puts methods.include?("erl_#{k}".to_sym)
+    methods.include?("erl_#{k}".to_sym) ? self.send("erl_#{k}".to_sym) : "#{k}"
+  end
+  
+  def opts;@opts ||= {};end
+  def start_commands;@commands.collect {|c| "-s #{c}"};end
+  def commands;@commands ||= [];end
+  
+  def method_missing(m, *args, &block)
+    command = "#{m}#{ " #{args.join(" ")}" unless args.empty?}"
+    commands.push command
   end
 end
 
-class MappingContext
-  attr_accessor :name, :string
-  
-  def initialize(name)
-    @name = name
-  end
-  
-  def take_options(value)
-    build_string += "-#{value[0]} #{value[1]} " if value.is_a?(Array)
-  end
-  
-  def build_string
-    @build_string ||= "erl "
+class Namespace < MappingContext
+  def start_commands
+    commands.collect {|c| "-s #{namespace}:#{c}"}
   end
 end
--- test/erlpers/erl_mapper_spec.rb
+++ test/erlpers/erl_mapper_spec.rb
@@ -6,28 +6,74 @@ class ErlMapperTest < Test::Unit::TestCase
       
       context "options" do
         
+        setup do;@mapper = ErlMapper.new do;a "a";b = nil;end;end
         describe "passed in with a hash" do
-          setup do
-            @mapper = ErlMapper.new do
-              options :a => "a", :b => nil
-            end
-          end
           it "should set the option on the ErlMapper" do;@mapper.a.should == "a";end
           it "should not set the option when passed a nil value" do;@mapper.b.should == nil;end
         end
-        describe "passed in on the block" do
-          setup do
-            @mapper = ErlMapper.new do
-              a "a"
-              b = nil
-            end
-          end
-          
+        describe "passed in on the block" do          
           it "should set the option on the ErlMapper" do;@mapper.a.should == "a";end
           it "should not set the option when passed a nil value" do;@mapper.b.should == nil;end
         end
       end
       
+      context "with_node" do
+        setup do;@mapper = ErlMapper.new do;with_node(:node0) do; "hi";end;end;end
+        describe "on call" do
+          it "should create a MappingContext" do
+            assert_equal @mapper.with_node(:node0).class, MappingContext
+          end
+          it "should store the MappingContext's name" do
+            assert_equal @mapper.with_node(:node0).sname, :node0
+          end
+          it "should append the name to the MappingContext's string" do
+            assert_equal @mapper.with_node(:node0).build_string, "erl -sname node0 -pa ./ebin "
+          end
+          
+          context "context" do
+            setup do;@context = @mapper.with_node(:node0) do;start;end;end
+            
+            describe "with start_commands" do            
+              it "should have the start_commands on the MappingContext" do              
+                assert_equal @context.commands, ["start"]
+              end
+              it "should have multiple when multiple are called" do
+                @context.instance_eval do
+                  chordjerl_srv:start
+                end
+                assert_equal @context.commands, ["start", "chordjerl_srv start"]
+              end
+            end
+            
+            describe "with final_commands" do
+              it "should have the default final_commands as -s init stop" do
+                assert_equal @context.final_commands, "-s init stop"
+              end
+              it "should not have the final_commands with stop if stop is false" do
+                cont = MappingContext.new :node1, {:stop => false} do                  
+                end
+                assert_equal cont.final_commands, ""
+              end
+              it "with stop if stop is true" do
+                cont = MappingContext.new :node2, {:stop => true} do                  
+                end
+                assert_equal cont.final_commands, "-s init stop"
+              end
+            end
+            
+          end
+          
+        end
+      end
+    end
+    
+    context "Namespace" do
+      setup do
+        @namespace = Namespace.new("chordjerl_srv", {:sname => "name_of_server"})
+      end
+      it "should keep the namespace on the command" do
+        assert_equal "erl -sname name_of_server", @namespace.string
+      end
     end
     
     describe "Called from an object" do
